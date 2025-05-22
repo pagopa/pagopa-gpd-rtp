b0157e083e033080dd6a43f51c7565e574359ace
svariant <samuele.varianti@nttdata.com>
2025-05-22 16:18:38 +0200

    chore: [PAGOPA-3061] Remove unused method

--- src/main/java/it/gov/pagopa/gpd/rtp/repository/PaymentOptionRepository.java
+++ src/main/java/it/gov/pagopa/gpd/rtp/repository/PaymentOptionRepository.java
@@ -7,7 +7,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentOptionRepository
-    extends JpaRepository<PaymentOption, Long>, JpaSpecificationExecutor<PaymentOption> {
-
-    PaymentOption findById(int id);
-}
+    extends JpaRepository<PaymentOption, Long>, JpaSpecificationExecutor<PaymentOption> { }
