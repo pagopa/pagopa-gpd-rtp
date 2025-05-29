a2e8a3480b4a4a6b5baafecbad0485306176e1d2
svariant <samuele.varianti@nttdata.com>
2025-05-29 10:23:05 +0200

    chore: [PAGOPA-3061] clean imports

--- src/test/java/it/gov/pagopa/gpd/rtp/ApplicationTest.java
+++ src/test/java/it/gov/pagopa/gpd/rtp/ApplicationTest.java
@@ -2,21 +2,7 @@ package it.gov.pagopa.gpd.rtp;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import com.azure.storage.blob.BlobServiceClient;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import it.gov.pagopa.gpd.rtp.client.AnonymizerClient;
-import it.gov.pagopa.gpd.rtp.client.BlobStorageClient;
-import it.gov.pagopa.gpd.rtp.events.producer.RTPMessageProducer;
-import it.gov.pagopa.gpd.rtp.repository.PaymentOptionRepository;
-import it.gov.pagopa.gpd.rtp.repository.TransferRepository;
-import it.gov.pagopa.gpd.rtp.service.DeadLetterService;
-import it.gov.pagopa.gpd.rtp.service.FilterService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.kafka.support.Acknowledgment;
 
 class ApplicationTest {
   @Test
