c5306e44a81076de6d1b433c3a025ea7b2644366
giomella <giomella@emeal.nttdata.com>
2025-07-03 16:07:48 +0200

    [PAGOPA-3163] fix

--- test-utils/run_compose.sh
+++ test-utils/run_compose.sh
@@ -4,4 +4,4 @@ export INGESTION_EVENTHUB_HOST=$1
 export INGESTION_EVENTHUB_CONN_STRING=$2
 export INGESTION_EVENTHUB_TOPICS=$3
 
-docker compose up -d --remove-orphans --force-recreate --build
+docker-compose up -d --remove-orphans --force-recreate --build
