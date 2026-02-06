75f0520b59a709b4af20d92d41c60f161af47f76
Angelo Caporaso <56113767+cap-ang@users.noreply.github.com>
2026-02-06 11:08:28 +0100

    fix(redis): add UUID to GROUP_NAME for fan-out
    Previously, only one microservice instance would receive commands because they shared the same group name. Appending a UUID ensures that each instance has a unique group, enabling proper fan-out logic so all containers receive the command.

--- src/main/java/it/gov/pagopa/gpd/rtp/util/Constants.java
+++ src/main/java/it/gov/pagopa/gpd/rtp/util/Constants.java
@@ -2,13 +2,15 @@ package it.gov.pagopa.gpd.rtp.util;
 
 import lombok.experimental.UtilityClass;
 
+import java.util.UUID;
+
 @UtilityClass
 public class Constants {
 
   public static final String HEADER_REQUEST_ID = "X-Request-Id";
   public static final String HEADER_SUBSCRIPTION_KEY = "Ocp-Apim-Subscription-Key";
   public static final String STREAM_KEY = "rtp-event-stream:action";
-  public static final String GROUP_NAME = "notification-group";
+  public static final String GROUP_NAME = "notification-group".concat(String.valueOf(UUID.randomUUID()));
   public static final String CONSUMER_NAME = "consumer-notifications-1";
   public static final String CUSTOM_EVENT = "RTP_ALERT";
 }
