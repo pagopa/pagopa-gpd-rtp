3ef6287bef714f8fc254c1fd8b6d839d530aaf5d
svariant <samuele.varianti@nttdata.com>
2026-03-25 15:03:10 +0100

    fix: [PIDM-1730] Add delete rtp_flag_optin cache key redis to replace values in a transaction operation

--- src/main/java/it/gov/pagopa/gpd/rtp/repository/RedisCacheRepository.java
+++ src/main/java/it/gov/pagopa/gpd/rtp/repository/RedisCacheRepository.java
@@ -5,10 +5,13 @@ import it.gov.pagopa.gpd.rtp.exception.FailAndNotify;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -24,10 +27,26 @@ public class RedisCacheRepository {
   private final StringRedisTemplate redisTemplate;
 
   public void saveAll(Collection<String> ids) {
-    redisTemplate.opsForSet().add(KEY, ids.toArray(new String[0]));
-    redisTemplate.opsForValue().set(CREATED_AT_KEY, LocalDateTime.now().toString());
-    redisTemplate.expire(KEY, TTL);
-    redisTemplate.expire(CREATED_AT_KEY, TTL);
+    // Redis transaction to prevent delete if save goes wrong
+    redisTemplate.execute(new SessionCallback<List<Object>>() {
+      @Override
+      public List<Object> execute(@NotNull RedisOperations operations) {
+        try {
+          operations.multi();
+
+          operations.delete(KEY);
+          operations.opsForSet().add(KEY, ids.toArray(String[]::new));
+          operations.opsForValue().set(CREATED_AT_KEY, LocalDateTime.now().toString());
+          operations.expire(KEY, TTL);
+          operations.expire(CREATED_AT_KEY, TTL);
+
+          return operations.exec();
+        } catch (RuntimeException e) {
+          operations.discard();
+          throw e;
+        }
+      }
+    });
   }
 
   public void setRetryCount(String id, int retryCount) {
