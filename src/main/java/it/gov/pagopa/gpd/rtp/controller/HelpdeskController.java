6b5552c3a6c7347e97e3e6220394a0895c5645c2
svariant <samuele.varianti@nttdata.com>
2026-04-13 16:03:35 +0200

    fix: [PIDM-1668] Missing import & update openapi

--- src/main/java/it/gov/pagopa/gpd/rtp/controller/HelpdeskController.java
+++ src/main/java/it/gov/pagopa/gpd/rtp/controller/HelpdeskController.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.gpd.rtp.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
